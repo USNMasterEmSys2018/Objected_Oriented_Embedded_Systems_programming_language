e13882498fba6e2a2b76375cc9bd945431dfa8bf
Bozhao Liu <liubozhao@gmail.com>
2018-09-21 13:28:57 +0200

    update_testing

--- VHDL/Task_intro3/Assignment2/BozhaoLiu/Oppgave1/Main_state_machine/Main_state_machine.srcs/sim_1/new/Test_RXTX_state machine.vhd
+++ VHDL/Task_intro3/Assignment2/BozhaoLiu/Oppgave1/Main_state_machine/Main_state_machine.srcs/sim_1/new/Test_RXTX_state machine.vhd
@@ -37,23 +37,32 @@ end Test_RXTX_state_machine;
 
 architecture Behavioral of Test_RXTX_state_machine is
       component RX_TX_read_write_control
+      Generic(Baud_width: integer := 1085);
       Port (
             clk: in std_logic;
             rx: in std_logic;
-            tx: out std_logic
+            tx: out std_logic;
+            sw: in std_logic_vector(0  downto 0);
+            led: out std_logic_vector(3  downto 0)
             );
     end component;
     
     signal clk : STD_LOGIC := '0';
     signal data_in, Data_out : STD_LOGIC := '0';
+    signal led:  std_logic_vector(3  downto 0);
     constant baudrate: integer := 115200;
-    constant Baud_width: time := 1000000000 ns/baudrate;
+    constant Baud_width: integer := 1000000000/baudrate/8;
+    constant Baud_width_time: time := Baud_width*8ns;
     constant datas: std_logic_vector(7 downto 0):="01010101";
     constant LineFeed: std_logic_vector(7 downto 0) :="00001010";
 begin
-    uut: RX_TX_read_write_control port map ( clk => clk,
+    uut: RX_TX_read_write_control 
+            Generic map(Baud_width => Baud_width)
+            port map ( clk => clk,
                                          rx  => data_in,
-                                         tx  => Data_out );
+                                         tx  => Data_out,
+                                         sw  => "1",
+                                         led => led );
     clock_control:process
     begin
         clk <= '0';
@@ -64,30 +73,54 @@ begin
     
     data_control:process
     begin
-        data_in <= '0';  
-        wait for Baud_width;
+        data_in <= '1';  
+        wait for Baud_width_time;
         --start bit
-        data_in <= '1';  
-        wait for Baud_width;
+        data_in <= '0';  
+        wait for Baud_width_time;
         --data 1
         for i in 7 downto 0 loop
-            data_in <= datas(i);  
-            wait for Baud_width;
+            data_in <= not(datas(i));  
+            wait for Baud_width_time;
         end loop;
         --pause bit
-        data_in <= '0';  
-        wait for Baud_width*3;
+        data_in <= '1';  
+        wait for Baud_width_time*3;
         --start LF
-        data_in <= '1';  
-        wait for Baud_width;
+        data_in <= '0';  
+        wait for Baud_width_time;
         --data 1
         for i in 7 downto 0 loop
-            data_in <= LineFeed(i);  
-            wait for Baud_width;
+            data_in <= not(LineFeed(i));  
+            wait for Baud_width_time;
         end loop;
         --pause bit
+        data_in <= '1';  
+        wait for Baud_width_time*40;
+                data_in <= '0';  
+        wait for Baud_width_time;
+        --start bit
         data_in <= '0';  
-        wait for Baud_width*40;
+        wait for Baud_width_time;
+        --data 1
+        for i in 7 downto 0 loop
+            data_in <= (datas(i));  
+            wait for Baud_width_time;
+        end loop;
+        --pause bit
+        data_in <= '1';  
+        wait for Baud_width_time*3;
+        --start LF
+        data_in <= '0';  
+        wait for Baud_width_time;
+        --data 1
+        for i in 7 downto 0 loop
+            data_in <= not(LineFeed(i));  
+            wait for Baud_width_time;
+        end loop;
+        --pause bit
+        data_in <= '1';  
+        wait for Baud_width_time*40;
     end process;
 
 end Behavioral;
--- VHDL/Task_intro3/Assignment2/BozhaoLiu/Oppgave1/Main_state_machine/Main_state_machine.srcs/sources_1/new/RX_TX_read_write_control.vhd
+++ VHDL/Task_intro3/Assignment2/BozhaoLiu/Oppgave1/Main_state_machine/Main_state_machine.srcs/sources_1/new/RX_TX_read_write_control.vhd
@@ -11,10 +11,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity RX_TX_read_write_control is
-  Port (
+    generic( Baud_width: integer := 1085);
+    Port (
         clk: in std_logic;
         rx: in std_logic;
-        tx: out std_logic
+        tx: out std_logic;
+        sw: in std_logic_vector(0  downto 0); --change mode and led output
+        led: out std_logic_vector(3  downto 0) --output the data controled by sw
         );
 end RX_TX_read_write_control;
 
@@ -60,34 +63,35 @@ architecture Behavioral of RX_TX_read_write_control is
                Data_out : out STD_LOGIC);
     end component;
     
-    signal read_cmd : STD_LOGIC := '0';
+    signal read_cmd, tx_buff, rx_buff : STD_LOGIC := '0';
     signal LF_read, LF_send : STD_LOGIC := '0';
     signal read_ctrl : STD_LOGIC := '0';
     signal send_ctrl : STD_LOGIC := '0';
-    signal Data_buff, data_buff2 : std_logic_vector(7 downto 0);
+    signal sta_in, sta_read_cmd, sta_LF_read, sta_LF_Send, sta_read_ctrl, sta_send_ctrl: std_logic := '0';
+    signal Data_buff, data_buff2, data_test : std_logic_vector(7 downto 0);
     signal address_reg, Address_to_read: STD_LOGIC_VECTOR ( 12 downto 0 ) := (others => '0');
     
     constant CLK_period: integer := 8;
     constant baudrate: integer := 115200;
-    constant Baud_width: integer := 1000000000/baudrate/clk_period;
+    
 begin
     main:Main_state_machine 
             port map(
                     clk => clk,
-                    data_in => rx,
+                    data_in => rx_buff,
                     read_cmd => read_cmd,
                     LF_Read => LF_read,
                     LF_send =>LF_Send,
                     read_ctrl => read_CTRL,
                     send_ctrl => send_ctrl
                     );
-                                    
+    rx_buff <= not(rx);                                
     data_read_in:Read_data_state_machine 
                     generic map(Baud_width => Baud_width
                                 )
                     port map(
                             clk => clk,
-                            data_in => rx,
+                            data_in => rx_buff,
                             read_control => read_ctrl,
                             LF => LF_read,
                             Read_cmd => Read_cmd,
@@ -111,6 +115,33 @@ begin
                              Data_in         => data_buff2,
                              Address_to_read => Address_to_read,
                              LF              => LF_send,
-                             Data_out        => tx );
-
+                             Data_out        => (tx_BUFF) );
+     TX <= not(Tx_buff) when sw ="1" else RX;
+     
+     process(clk, rx_buff, read_cmd, LF_read, LF_send, read_ctrl, send_ctrl)
+     begin
+        if rising_edge(clk) then
+            if rx_buff = '1'then
+                sta_in <= '1';
+            end if;
+            if read_cmd = '1' then
+                sta_read_cmd <= '1';
+                data_test <= Data_buff;
+            end if;
+            if LF_read = '1' then
+                sta_LF_read <= '1';
+            end if;
+            if LF_send = '1' then
+                sta_LF_Send <= '1';
+            end if;
+            if read_ctrl = '1' then
+                sta_read_ctrl <= '1';
+            end if;
+            if send_ctrl = '1' then 
+                sta_send_ctrl <= '1';
+            end if;
+        end if;
+    end process;
+    led <= Data_buff(7 downto 4) when sw = "1" else
+           Data_buff(3 downto 0) ;
 end Behavioral;
